461314edb9c007bc442b772544314fcab2e8f26f
ISMAEL SAGREDO <sagredo.ismael@live.com>
2019-09-26 15:14:01 +0200

    add UI to outputDisplay

--- src/components/display/outputDisplay.js
+++ src/components/display/outputDisplay.js
@@ -1,36 +1,86 @@
 import React from 'react'
 import styled from 'styled-components'
+import { View, Text } from 'react-native'
 import Constants from '../../utils/constants'
 import {sizeNormalize} from '../../utils/adaptSizes'
 import { Col, Row, Grid } from "react-native-easy-grid"
 
 const Wrapper = styled.View`
   display: flex;
-  flex:1;
-  align-items: center;
+  flexDirection: row;
+  alignSelf: center;
 `
 
-const NumberWrapper = styled.Text`
+const NumberWrapper = styled.View`
   display: flex;
-  flex:1;
-  font-size: ${sizeNormalize(20)};
+  alignItems: center;
+`
+
+const Number = styled.Text`
+  display: flex;
+  font-size: ${sizeNormalize(100)};
   color: ${Constants.numberColor};
 `
 
-export const NumberDisplay = ({numbersOutput}) => {
+const DecimalWrapper = styled.View`
+  display: flex;
+  alignItems: flex-start;
+`
+
+const Decimal = styled.Text`
+  display: flex;
+  justifyContent: flex-start;
+  font-size: ${sizeNormalize(40)};
+  color: ${Constants.euroIconColor};
+`
+
+const EuroWrapper = styled.View`
+  display: flex;
+  alignItems: flex-end;
+`
+
+const Euro = styled.Text`
+  display: flex;
+  textAlign: right;
+  fontWeight: bold;
+  font-size: ${sizeNormalize(60)};
+  color: ${Constants.euroIconColor};
+`
+
+export const NumberDisplay = ({numbersOutput, numbersDecimal}) => {
   return (
     <Grid>
       <Row size={20}/>
       <Row size={60}>
         <Col size={10}/>
-        <Col size={80}>
+        <Col size={75}>
           <Wrapper>
+            <EuroWrapper>
+              <Row size={10}/>
+              <Row size={70}>
+                <Euro>
+                  €
+                </Euro>
+              </Row>
+              <Row size={20}/>
+            </EuroWrapper>
             <NumberWrapper>
-              {numbersOutput}
+              <Number numberOfLines={1} ellipsizeMode="clip">
+                {numbersOutput}
+              </Number>
             </NumberWrapper>
+            <DecimalWrapper>
+              <Row size={15}/>
+              <Row size={65}>
+                <Decimal numberOfLines={1}>
+                {numbersOutput}
+                </Decimal>
+              </Row>
+              <Row size={20}/>
+            </DecimalWrapper>
           </Wrapper>
         </Col>
-        <Col size={10}/>
+        <Col size={15}/>
       </Row>
       <Row size={20}/>
     </Grid>
